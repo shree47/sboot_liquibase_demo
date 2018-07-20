a94fbf66bbb322650a155ab00218e803a822090b
devopsevd <devopsevd@users.noreply.github.com>
2018-07-20 13:14:12 +0530

    Update SbootLiquibaseDemoApplicationTests.java

--- src/test/java/com/premierinc/sboot/demo/SbootLiquibaseDemoApplicationTests.java
+++ src/test/java/com/premierinc/sboot/demo/SbootLiquibaseDemoApplicationTests.java
@@ -6,7 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(
+	classes = SbootLiquibaseDemoApplication.class
+)
 public class SbootLiquibaseDemoApplicationTests {
 
 	@Test

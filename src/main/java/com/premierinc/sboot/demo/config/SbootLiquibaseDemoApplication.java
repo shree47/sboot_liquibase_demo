363d8438584c2d1f13770df95952c3419fde1c2b
shree47 <35099650+shree47@users.noreply.github.com>
2018-09-26 15:31:25 +0530

    added more imports

--- src/main/java/com/premierinc/sboot/demo/config/SbootLiquibaseDemoApplication.java
+++ src/main/java/com/premierinc/sboot/demo/config/SbootLiquibaseDemoApplication.java
@@ -1,8 +1,14 @@
+
 package com.premierinc.sboot.demo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+////////////////////////// ADDING NEW IMPORTS////////////////////////////
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+///////////////////////////////////////////////////////////////////////////
+
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;

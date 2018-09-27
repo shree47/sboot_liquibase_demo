3d4fbf1973460194d68f67f33025d77c3023b636
shree47 <35099650+shree47@users.noreply.github.com>
2018-09-27 12:26:36 +0530

    added class name

--- src/test/java/com/premierinc/sboot/demo/SbootLiquibaseDemoApplicationTests.java
+++ src/test/java/com/premierinc/sboot/demo/SbootLiquibaseDemoApplicationTests.java
@@ -1,5 +1,6 @@
 
 
+
 package com.premierinc.sboot.demo;
 
 import org.junit.Test;
@@ -9,10 +10,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.premierinc.sboot.demo.config.SbootLiquibaseDemoApplication;
 
 //@RunWith(SpringRunner.class)
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=SbootLiquibaseDemoApplication.class)
 public class SbootLiquibaseDemoApplicationTests {
 
 	@Test

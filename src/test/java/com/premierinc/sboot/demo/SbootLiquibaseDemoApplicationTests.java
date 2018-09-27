217fa79fdd017b4153b8a2ef225701f13b285eca
shree47 <35099650+shree47@users.noreply.github.com>
2018-09-27 11:50:25 +0530

    removed new added import

--- src/test/java/com/premierinc/sboot/demo/SbootLiquibaseDemoApplicationTests.java
+++ src/test/java/com/premierinc/sboot/demo/SbootLiquibaseDemoApplicationTests.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.context.annotation.Configuration;
-
 
 @RunWith(SpringRunner.class)
 @SpringBootTest

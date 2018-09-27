56fd185e0e98062be617d3b652bae4d9ad03577c
shree47 <35099650+shree47@users.noreply.github.com>
2018-09-27 11:54:04 +0530

    added import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;

--- src/test/java/com/premierinc/sboot/demo/SbootLiquibaseDemoApplicationTests.java
+++ src/test/java/com/premierinc/sboot/demo/SbootLiquibaseDemoApplicationTests.java
@@ -1,10 +1,13 @@
+
 package com.premierinc.sboot.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
